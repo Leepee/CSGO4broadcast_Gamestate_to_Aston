ea9c58f162c5ddcfc5b94f9583c246a93763bdf4
Lee Davison <lee.davison@solent.ac.uk>
2021-12-09 15:15:32 +0000

    Refactor of code to pull allplayers > weapon data. Manual parsing of all entries so that they land in the right places and don't move! Will still throw errors if a datapoint becomes missing.

--- payloadparser.py
+++ payloadparser.py
@@ -5,6 +5,27 @@ from shutil import copyfile
 
 # A re-written function to manually build out a CSV that's simple to deal with from the user's side
 class PayloadParser:
+
+    # def geddit (self, dict, *keys):
+    #
+    #     print(keys)
+    #
+    #     for key in keys:
+    #         try:
+    #             print(key)
+    #             return dict.get(key)
+    #             # dict = dict[key]
+    #             # print(_dict)
+    #         except KeyError:
+    #             return 'n/a'
+    #
+    #
+    #     # try:
+    #     #     return dict[keys]
+    #     # except:
+    #     #     return 'n/a'
+
+
     def parse_payload(self, payload, gamestate):
 
         # print(payload)
@@ -21,9 +42,12 @@ class PayloadParser:
                       'weapon 0 name', 'weapon 0 paintkit', 'weapon 0 type', 'weapon 0 state', 'weapon 0 max ammo', 'weapon 0 ammo reserve', 'weapon 0 state',
                       'weapon 1 name', 'weapon 1 paintkit', 'weapon 1 type', 'weapon 1 ammo', 'weapon 1 max ammo', 'weapon 1 ammo reserve', 'weapon 1 state',
                       'weapon 2 name', 'weapon 2 paintkit', 'weapon 2 type', 'weapon 2 ammo', 'weapon 2 max ammo', 'weapon 2 ammo reserve', 'weapon 2 state',
-                      'weapon 3 name', 'weapon 3 paintkit', 'weapon 3 type', 'weapon 3 state',
-                      'weapon 4 name', 'weapon 4 paintkit', 'weapon 4 type', 'weapon 4 state',
-                      'weapon 5 name', 'weapon 5 paintkit', 'weapon 5 type', 'weapon 5 state'
+                      'weapon 3 name', 'weapon 3 paintkit', 'weapon 3 type', 'weapon 3 state', 'weapon 3 max ammo', 'weapon 3 ammo reserve', 'weapon 3 state',
+                      'weapon 4 name', 'weapon 4 paintkit', 'weapon 4 type', 'weapon 4 state', 'weapon 4 max ammo', 'weapon 4 ammo reserve', 'weapon 4 state',
+                      'weapon 5 name', 'weapon 5 paintkit', 'weapon 5 type', 'weapon 5 state', 'weapon 5 max ammo', 'weapon 5 ammo reserve', 'weapon 5 state',
+                      'weapon 6 name', 'weapon 6 paintkit', 'weapon 6 type', 'weapon 6 state', 'weapon 6 max ammo', 'weapon 6 ammo reserve', 'weapon 6 state',
+                      'weapon 7 name', 'weapon 5 paintkit', 'weapon 7 type', 'weapon 7 state', 'weapon 7 max ammo', 'weapon 7 ammo reserve', 'weapon 7 state',
+                      'weapon 8 name', 'weapon 8 paintkit', 'weapon 8 type', 'weapon 8 state', 'weapon 8 max ammo', 'weapon 8 ammo reserve', 'weapon 8 state'
                       ]
 
         f = open('data_out.csv', 'w', newline='', encoding='utf-8')
@@ -86,9 +110,134 @@ class PayloadParser:
                              data['allplayers'][playerID]['state']['flashed'], data['allplayers'][playerID]['state']['burning'], data['allplayers'][playerID]['state']['money'],
                              data['allplayers'][playerID]['state']['round_kills'], data['allplayers'][playerID]['state']['round_killhs'], data['allplayers'][playerID]['state']['round_totaldmg'], data['allplayers'][playerID]['state']['equip_value']])
 
-            for guns in data['allplayers'][playerID]['weapons']:
-                for gundata in data['allplayers'][playerID]['weapons'][guns]:
-                    allplayerRow.append(data['allplayers'][playerID]['weapons'][guns][gundata])
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            # Weapon 0 - knife data
+            print(data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_0').get('state', 'n/a'))
+
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_1').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_1').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_1').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_1').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_1').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_1').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_1').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_1').get('state', 'n/a'))
+
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_2').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_2').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_2').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_2').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_2').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_2').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_2').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_2').get('state', 'n/a'))
+
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_3').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_3').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_3').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_3').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_3').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_3').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_3').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_3').get('state', 'n/a'))
+
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_4').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_4').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_4').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_4').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_4').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_4').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_4').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_4').get('state', 'n/a'))
+
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_5').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_5').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_5').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_5').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_5').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_5').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_5').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_5').get('state', 'n/a'))
+
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_6').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_6').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_6').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_6').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_6').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_6').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_6').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_6').get('state', 'n/a'))
+
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_7').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_7').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_7').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_7').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_7').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_7').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_7').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_7').get('state', 'n/a'))
+
+            try:
+                data.get('allplayers').get(playerID).get('weapons').get('weapon_8').get('name', 'n/a')
+            except AttributeError:
+                writer.writerow(allplayerRow)
+                continue
+
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_8').get('name', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_8').get('paintkit', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_8').get('type', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_8').get('ammo_clip', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_8').get('ammo_clip_max', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_8').get('ammo_reserve', 'n/a'))
+            allplayerRow.append(data.get('allplayers').get(playerID).get('weapons').get('weapon_8').get('state', 'n/a'))
+
 
             writer.writerow(allplayerRow)
 
